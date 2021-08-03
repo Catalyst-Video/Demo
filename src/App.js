09d3e69f1320e12d69c06ce30c23f627ba74ad72
Seth <sethgoldin27@gmail.com>
2021-08-03 04:14:58 -0400

    bump catalyst

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import CatalystChat from "catalyst-vc-react";
 
 function App() {
+	if(process.env.REACT_APP_APP_ID)
   return (
-	<CatalystChat
-			room="Test Room"
-			appId="YOUR_CATALYST_PROJECT_ID"
-	// 		theme={{
-	// 			primary: "#50c878",
-	// 			primaryDark: "#50c878",
-    //   }}
+		<CatalystChat
+			room="catalyst-demo"
+			appId={process.env.REACT_APP_APP_ID}
+			// 		theme={{
+			// 			primary: "#50c878",
+			// 			primaryDark: "#50c878",
+			//   }}
 		/>
-	);
+  );
+	else return null
 }
 
 export default App;

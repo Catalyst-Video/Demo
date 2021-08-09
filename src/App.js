a68c6dcedfcd9513ade1335d5af9de0d6c0f40c1
Seth <sethgoldin27@gmail.com>
2021-08-09 02:22:17 -0400

    add url

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import CatalystChat from "catalyst-vc-react";
 
 function App() {
-	if(process.env.REACT_APP_APP_ID)
-  return (
-		<CatalystChat
-			room="catalyst-demo"
-			appId={process.env.REACT_APP_APP_ID}
+	if (process.env.REACT_APP_APP_ID)
+		return (
+			<CatalystChat
+				room="catalyst-demo"
+				appId={process.env.REACT_APP_APP_ID}
+				onLeaveCall={() => {
+					window.open("https://catalyst.chat/");
+				}}
 			// 		theme={{
 			// 			primary: "#50c878",
 			// 			primaryDark: "#50c878",
